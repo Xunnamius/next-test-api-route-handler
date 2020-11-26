accf33d5d4b3f4c0624cd5eebffcd19578ece972
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2020-11-26 09:06:18 -0800

    chore: ready for semantic-release pipeline

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-    collectCoverageFrom: [
-        'src/**/*.ts',
-        'external-scripts/**/*.ts',
-    ],
     testEnvironment: 'node',
     testRunner: 'jest-circus/runner',
     verbose: false,
