d3cfbadf5e4987dffb843c4c5130cbdf37b62c33
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2021-02-21 20:33:07 -0800

    refactor(integration-external.test.ts): remove fulfilled TODOs

--- test/integration-external.test.ts
+++ test/integration-external.test.ts
@@ -11,7 +11,6 @@ const debugId = `${pkgName}:*`;
 const debug = debugFactory(`${pkgName}:${TEST_IDENTIFIER}`);
 const runExternal = runnerFactory('node', [EXTERNAL_BIN_PATH]);
 
-// TODO: configure automatically generated test fixtures
 const fixtureOptions = {
   initialFileContents: {
     'package.json': `{
@@ -31,7 +30,6 @@ const fixtureOptions = {
 const withMockedFixture = mockFixtureFactory(TEST_IDENTIFIER, fixtureOptions);
 
 beforeAll(async () => {
-  // TODO: test environment to ensure expected files and executables are present
   if ((await run('test', ['-e', EXTERNAL_BIN_PATH])).code != 0) {
     debug(`unable to find external executable: ${EXTERNAL_BIN_PATH}`);
     throw new Error('must build externals first (try `npm run build-externals`)');
@@ -48,6 +46,20 @@ it(`runs silent by default`, async () => {
     expect(stdout).toBeEmpty();
     // eslint-disable-next-line jest/no-conditional-expect
     !process.env.DEBUG && expect(stderr).toBeEmpty();
+  });
+});
+
+it(`is verbose when DEBUG='${debugId}'`, async () => {
+  expect.hasAssertions();
+
+  await withMockedFixture(async () => {
+    const { code, stdout, stderr } = await runExternal(undefined, {
+      env: { DEBUG: debugId }
+    });
+
+    expect(code).toBe(0);
+    expect(stdout).toBeEmpty();
+    expect(stderr).toStrictEqual(expect.stringContaining('execution complete'));
   });
 });
 
