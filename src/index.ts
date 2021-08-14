1c3425caf7d80793a2c1e88ff8fbd29ada8adf2d
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2021-08-14 00:17:58 -0700

    build(src): add descriptions to TypeScript types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-unresolved, @typescript-eslint/prefer-ts-expect-error, @typescript-eslint/ban-ts-comment */
 import listen from 'test-listen';
 import fetch from 'isomorphic-unfetch';
 import { createServer } from 'http';
@@ -7,25 +8,61 @@ import type { NextApiHandler } from 'next';
 import type { IncomingMessage, ServerResponse } from 'http';
 
 /**
+ * The parameters expected by `testApiHandler`.
+ */
+export type Parameters<NextApiHandlerType = unknown> = {
+  /**
+   * A function that receives an `IncomingMessage` object. Use this function to
+   * edit the request before it's injected into the handler.
+   */
+  requestPatcher?: (req: IncomingMessage) => void;
+  /**
+   * A function that receives a `ServerResponse` object. Use this functions to
+   * edit the request before it's injected into the handler.
+   */
+  responsePatcher?: (res: ServerResponse) => void;
+  /**
+   * A function that receives an object representing "processed" dynamic routes;
+   * _modifications_ to this object are passed directly to the handler. This
+   * should not be confused with query string parsing, which is handled
+   * automatically.
+   */
+  paramsPatcher?: (params: Record<string, unknown>) => void;
+  /**
+   * `params` is passed directly to the handler and represent processed dynamic
+   * routes. This should not be confused with query string parsing, which is
+   * handled automatically.
+   *
+   * `params: { id: 'some-id' }` is shorthand for `paramsPatcher: (params) =>
+   * (params.id = 'some-id')`. This is most useful for quickly setting many
+   * params at once.
+   */
+  params?: Record<string, string | string[]>;
+  /**
+   * `url: 'your-url'` is shorthand for `requestPatcher: (req) => (req.url =
+   * 'your-url')`
+   */
+  url?: string;
+  /**
+   * The actual handler under test. It should be an async function that accepts
+   * `NextApiRequest` and `NextApiResult` objects (in that order) as its two
+   * parameters.
+   */
+  handler: NextApiHandler<NextApiHandlerType>;
+  /**
+   * `test` must be a function that runs your test assertions, returning a
+   * promise (or async). This function receives one parameter: `fetch`, which is
+   * the unfetch package's `fetch(...)` function but with the first parameter
+   * omitted.
+   */
+  test: (obj: {
+    fetch: (init?: RequestInit) => ReturnType<typeof fetch>;
+  }) => Promise<void>;
+};
+
+/**
  * Uses Next's internal `apiResolver` to execute api route handlers in a
  * Next-like testing environment.
- *
- * `requestPatcher/responsePatcher` are functions that receive an
- * IncomingMessage and ServerResponse object respectively. Use these functions
- * to edit the request and response before they're injected into the api
- * handler.
- *
- * `params` are passed directly to the api handler and represent processed
- * dynamic routes. This should not be confused with query string parsing, which
- * is handled automatically.
- *
- * `handler` is the actual api handler under test. It should be an async
- * function that accepts NextApiRequest and NextApiResult objects (in that
- * order) as its two parameters.
- *
- * `test` should be a function that returns a promise (or async) where test
- * assertions can be run. This function receives one parameter: fetch, which is
- * unfetch's `fetch(...)` function but with the first parameter omitted.
  */
 export async function testApiHandler<NextApiHandlerType = unknown>({
   requestPatcher,
@@ -35,17 +72,7 @@ export async function testApiHandler<NextApiHandlerType = unknown>({
   url,
   handler,
   test
-}: {
-  requestPatcher?: (req: IncomingMessage) => void;
-  responsePatcher?: (res: ServerResponse) => void;
-  paramsPatcher?: (params: Record<string, unknown>) => void;
-  params?: Record<string, string | string[]>;
-  url?: string;
-  handler: NextApiHandler<NextApiHandlerType>;
-  test: (obj: {
-    fetch: (init?: RequestInit) => ReturnType<typeof fetch>;
-  }) => Promise<void>;
-}) {
+}: Parameters<NextApiHandlerType>) {
   let server = null;
 
   try {
