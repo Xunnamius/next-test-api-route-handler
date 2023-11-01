6b9f9b7d5eaf8352f12c3967ee1af9f00455f6d5
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2023-11-01 15:37:28 -0700

    chore(jest): use latest global jest config

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,28 @@
 'use strict';
 
+/**
+ * @type {import('jest').Config}
+ */
 module.exports = {
   restoreMocks: true,
   resetMocks: true,
   testEnvironment: 'node',
   testRunner: 'jest-circus/runner',
-  // ? 1 hour so MMS and other tools don't choke during debugging
-  testTimeout: 60000,
+  // ? 24h if debugging so MMS and other tools don't choke, otherwise 1m
+  testTimeout:
+    1000 *
+    60 *
+    (process.env.VSCODE_INSPECTOR_OPTIONS
+      ? 60 * 24
+      : process.platform == 'win32'
+      ? 5
+      : 1),
+  // ? Minimum of 2 concurrent tests executed at once; maximum of cpu cores - 1
+  maxConcurrency: Math.max(require('node:os').cpus().length - 1, 2),
   verbose: false,
-  testPathIgnorePatterns: ['/node_modules/'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   // ! If changed, also update these aliases in tsconfig.json,
-  // ! webpack.config.js, next.config.ts, and .eslintrc.js
+  // ! babel.config.js, and .eslintrc.js
   moduleNameMapper: {
     '^universe/(.*)$': '<rootDir>/src/$1',
     '^multiverse/(.*)$': '<rootDir>/lib/$1',
@@ -20,5 +32,7 @@ module.exports = {
     '^package$': '<rootDir>/package.json'
   },
   setupFilesAfterEnv: ['./test/setup.ts'],
-  collectCoverageFrom: ['src/**/*.ts?(x)', 'external-scripts/**/*.ts?(x)']
+  collectCoverageFrom: ['src/**/*.ts?(x)', 'external-scripts/**/*.ts?(x)'],
+  // ? Make sure jest-haste-map doesn't try to parse and cache fixtures
+  modulePathIgnorePatterns: ['<rootDir>/test/fixtures']
 };
