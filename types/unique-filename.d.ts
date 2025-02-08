634d9bb5a816882301096acd2d66bfd2e5bf4b1b
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2025-02-08 12:43:06 -0800

    refactor(types): remove unused types

--- types/unique-filename.d.ts
+++
@@ -1,3 +0,0 @@
-declare module 'unique-filename' {
-  export default function (dir: string, filePrefix?: string, uniqStr?: string): string;
-}
--- types/webpack.d.ts
+++
@@ -1,2 +0,0 @@
-declare const __webpack_require__: typeof require;
-declare const __non_webpack_require__: typeof require;
