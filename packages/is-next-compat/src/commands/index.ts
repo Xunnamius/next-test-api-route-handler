aaef599d2edd808506ad2c38e5239ac77302df94
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2025-09-03 19:30:57 -0700

    ci(packages/is-next-compat): implement cli tool

--- packages/is-next-compat/src/commands/index.ts
+++ packages/is-next-compat/src/commands/index.ts
@@ -12,12 +12,105 @@ import {
   version as ntarhPackageVersion
 } from 'rootverse:package.json';
 
+import type { RootConfiguration } from '@black-flag/core';
+
 const log = createGenericLogger({ namespace: 'is-next-compat' });
 const debug = createDebugLogger({ namespace: 'is-next-compat' });
 
-log(`internal NTARH package: ${ntarhPackageName}@${ntarhPackageVersion}`);
+export const description: RootConfiguration['description'] = `
+This tool looks for a \`_is_next_compat_test_mode\` npm script with a zero exit code. If found, no DB connections will be made. Said script should appear in integration tests' package files to prevent those tests from making DB connections using a project's (potentially production) .env values.
 
-// TODO: issue warning when running if not in CI: don't run this in the real repo dir, but in a duplicate temp dir
+Example:
+
+{
+  "scripts": {
+    "_is_next_compat_test_mode": "true"
+  }
+}
+`.trim();
+
+export const handler: RootConfiguration['handler'] = async () => {
+  const cwd = getCurrentWorkingDirectory();
+
+  if (!process.env.CI && cwd.includes('/repos/next-test-api-route-handler')) {
+    log.warn(
+      'It is dangerous to run this CLI tool in the actual NTARH repository directory. Consider running this tool in a temporary clone instead'
+    );
+  }
+
+  log(`internal NTARH package: ${ntarhPackageName}@${ntarhPackageVersion}`);
+  log('connecting to GitHub');
+
+  if (!process.env.GH_TOKEN) {
+    log('warning: not using a personal access token!');
+  }
+
+  const { Octokit } = await import('@octokit/rest');
+
+  const { repos } = new Octokit({
+    auth: process.env.GH_TOKEN,
+    userAgent: `${ntarhPackageName}@${ntarhPackageVersion}`
+  });
+
+  const {
+    data: { tag_name: latestVersion }
+  } = await repos.getLatestRelease({
+    owner: 'vercel',
+    repo: 'next.js'
+  });
+
+  const latestReleaseVersion = latestVersion.replace(/^v/, '');
+  log(`saw latest release version "${latestReleaseVersion}"`);
+
+  if (!latestReleaseVersion) {
+    throw new Error('could not find latest Next.js version');
+  }
+
+  const { filename: path } = findPackageJson(getCurrentWorkingDirectory()).next();
+  log(`using path: %O`, path);
+
+  if (!path) {
+    throw new Error('could not find package.json');
+  }
+
+  const nextVersionUnderTestFullNameAndVersion = `next@${latestReleaseVersion}`;
+
+  log('installing %O for unit tests', nextVersionUnderTestFullNameAndVersion);
+  log('(integration tests use their own Next.js versions)');
+
+  // ? Install peer deps manually for Next.js
+  const nextLatestReleaseVersionPeerDependencies = await getNextjsReactPeerDependencies(
+    nextVersionUnderTestFullNameAndVersion
+  );
+
+  await run('npm', [
+    'install',
+    '--no-save',
+    '--force',
+    nextVersionUnderTestFullNameAndVersion,
+    ...nextLatestReleaseVersionPeerDependencies
+  ]);
+
+  log('attempting to repair node_modules after install using %O', 'npm run prepare');
+
+  const { exitCode, all } = await runNoRejectOnBadExit('npm', ['run', 'prepare'], {
+    all: true
+  });
+
+  if (exitCode !== 0) {
+    debug.warn('%O command did not complete successfully: %O', 'npm run prepare', all);
+  }
+
+  log('running compatibility tests');
+
+  await run('npm', ['run', 'test:packages:all']);
+
+  log('test succeeded');
+
+  await setCompatFlagTo(latestReleaseVersion);
+
+  log('execution complete');
+};
 
 /**
  * Detect if this tool was invoked in the context of an integration test
@@ -74,90 +167,4 @@ async function setCompatFlagTo(version: string) {
     log.warn('an attempt to update the database failed');
     throw error;
   }
-}
-
-/**
- * The is-next-compat runtime.
- *
- * This tool looks for a `_is_next_compat_test_mode` npm script with a zero exit
- * code. If found, no DB connections will be made. Should appear in integration
- * tests' package files to prevent those tests from making DB connections using
- * a project's (potentially production) .env values.
- *
- * ```
- * "scripts": {
- *   "_is_next_compat_test_mode": "true"
- * }
- * ```
- */
-async function main() {
-  log('connecting to GitHub');
-
-  if (!process.env.GH_TOKEN) log('warning: not using a personal access token!');
-
-  const { Octokit } = await import('@octokit/rest');
-
-  const { repos } = new Octokit({
-    auth: process.env.GH_TOKEN,
-    userAgent: `${ntarhPackageName}@${ntarhPackageVersion}`
-  });
-
-  const {
-    data: { tag_name: latestVersion }
-  } = await repos.getLatestRelease({
-    owner: 'vercel',
-    repo: 'next.js'
-  });
-
-  const latestReleaseVersion = latestVersion.replace(/^v/, '');
-  log(`saw latest release version "${latestReleaseVersion}"`);
-
-  if (!latestReleaseVersion) throw new Error('could not find latest Next.js version');
-
-  const { filename: path } = findPackageJson(getCurrentWorkingDirectory()).next();
-  log(`using path: %O`, path);
-
-  if (!path) {
-    throw new Error('could not find package.json');
-  }
-
-  const nextVersionUnderTestFullNameAndVersion = `next@${latestReleaseVersion}`;
-
-  log('installing %O for unit tests', nextVersionUnderTestFullNameAndVersion);
-  log('(integration tests use their own Next.js versions)');
-
-  // ? Install peer deps manually for Next.js
-  const nextLatestReleaseVersionPeerDependencies = await getNextjsReactPeerDependencies(
-    nextVersionUnderTestFullNameAndVersion
-  );
-
-  await run('npm', [
-    'install',
-    '--no-save',
-    '--force',
-    nextVersionUnderTestFullNameAndVersion,
-    ...nextLatestReleaseVersionPeerDependencies
-  ]);
-
-  log('attempting to repair node_modules after install using %O', 'npm run prepare');
-
-  const { exitCode, all } = await runNoRejectOnBadExit('npm', ['run', 'prepare'], {
-    all: true
-  });
-
-  if (exitCode !== 0) {
-    debug.warn('%O command did not complete successfully: %O', 'npm run prepare', all);
-  }
-
-  log('running compatibility tests');
-
-  await run('npm', ['run', 'test:packages:all']);
-
-  log('test succeeded');
-
-  await setCompatFlagTo(latestReleaseVersion);
-
-  log('execution complete');
-
-  process.exitCode = 0;
 }
